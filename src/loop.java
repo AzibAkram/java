9d0fe770cb6ecacc42b435e4a78abc03f655d168
AzibAkram <m.azibakram@gmail.com>
2024-07-15 04:22:04 -0700

    largest number

---
+++ src/loop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        System.out.println("enter the number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=1;i<=num;i++){
+            System.out.println("hellow");
+        }
+        {
+            if(num==0){
+                System.out.println("zero");
+            }
+        }
+
+        }
+
+
+    }
+
